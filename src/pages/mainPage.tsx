fe5dc46b41a93b4e151d6dc2d7a4d86e85792237
unknown <ballogggamer@gmail.com>
2024-08-31 18:00:19 -0300

    add Mainpage component

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,26 @@
+import "../styles/mainPage.css"
+
+const MainPage = function () {
+    return (
+        <>
+            <div className="middle">
+                <div className="middle_button">
+                    <button className="filters_button">Criar</button>
+                </div>
+            </div>
+
+            <div className="list_of_quizes">
+                {/* { */}
+                    <div>egsig</div>
+                    <div>egsig</div>
+                    <div>egsig</div>
+                    <div>egsig</div>
+                    <div>egsig</div>
+                    <div>egsig</div>
+                {/* } */}
+            </div>
+        </>
+    )
+}
+
+export default MainPage
